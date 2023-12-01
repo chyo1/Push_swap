633567eabeb8fdcafc66a6e1a15c6c10fcc022a3
Hyowon Choi <hyowchoi@c2r6s7.42seoul.kr>
2023-12-01 18:07:49 +0900

    fix_14 16 17

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hyowchoi <hyowchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 17:19:41 by hyowchoi          #+#    #+#             */
-/*   Updated: 2023/12/01 16:52:24 by hyowchoi         ###   ########.fr       */
+/*   Updated: 2023/12/01 18:07:13 by hyowchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,27 +34,15 @@ int	main(int argc, char **argv)
 		}
 		i++;
 	}
-	if (cnt == 14)
-	{
-		make_node(&head_a, INT_MIN);
-		cnt++;
-	}
-	else if (cnt == 16 || cnt == 17)
-	{
-		make_node(&head_a, INT_MIN);
-		make_node(&head_a, INT_MIN);
-		cnt += 2;
-	}
 	init_div(&head_a, &head_b, cnt, ASC);
-	// print_list(&head_b); //
 	merge(&head_a, &head_b, cnt, 1);
-	//merge_to_a(&head_a, &head_b, 1);
-	if (cnt == 14 || cnt == 16)
-		pop_front(&head_a);
-	else if (cnt == 17)
+	if (head_a == NULL)
 	{
-		pop_front(&head_a);
-		pop_front(&head_a);
+		while (head_b)
+		{
+			reverse_rotate(&head_a, &head_b, 'b');
+			push_a(&head_a, &head_b);
+		}
 	}
 }
 
